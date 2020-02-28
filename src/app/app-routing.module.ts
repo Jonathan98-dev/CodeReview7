dadde3b531df7e95c46ce35a6094497de216f57a
Jonathan Nuel <email.jonathannuel@gmail.com>
2020-02-28 09:47:44 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//my route components
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { TravelComponent } from './travel/travel.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ 
+		path: '', component: HomeComponent
+	},
+	{ 
+		path: 'blog', component: BlogComponent
+	},
+	{ 
+		path: 'travels', component: TravelComponent
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { TravelComponent } from './travel/travel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    TravelComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
