a22de3d6f79ff1d8d93162b845ab3e0c22fe1562
Jonathan Nuel <email.jonathannuel@gmail.com>
2020-02-28 18:15:27 +0100

    a

--- src/app/travel/travel.component.ts
+++ src/app/travel/travel.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from '../cart.service'
+import { travels } from './travels'
 @Component({
   selector: 'app-travel',
   templateUrl: './travel.component.html',
   styleUrls: ['./travel.component.scss']
 })
 export class TravelComponent implements OnInit {
+	travels=travels;
 
-  constructor() { }
+	description:boolean = false;
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+	
+	descshow()
+	{
+		
+		this.description=!this.description;
+
+	};
+
+
+	ngOnInit()
+	{
+
+	}
 
 }
---
+++ src/app/travel/travels.ts
@@ -0,0 +1,38 @@
+export const travels = [
+ {
+   destination: 'Travel Destination',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+];
