c6e373ca2cfd6a72b2b6d683cf55405b9661539a
Jonathan Nuel <email.jonathannuel@gmail.com>
2020-02-29 15:08:47 +0100

    almost done

--- src/app/travel/travels.ts
+++ src/app/travel/travels.ts
@@ -1,6 +1,6 @@
 export const travels = [
  {
-   destination: 'Travel Destination',
+   destination: 'Travel Destination1',
    date1: "01.01.2020",
    date2: "02.02.2020",
    price: 200,
@@ -9,7 +9,7 @@ export const travels = [
    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
  },
  {
-   destination: 'Travel Destination',
+   destination: 'Travel Destination2',
    date1: "01.01.2020",
    date2: "02.02.2020",
    price: 200,
@@ -18,7 +18,7 @@ export const travels = [
    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
  },
  {
-   destination: 'Travel Destination',
+   destination: 'Travel Destination3',
    date1: "01.01.2020",
    date2: "02.02.2020",
    price: 200,
@@ -27,7 +27,61 @@ export const travels = [
    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
  },
  {
-   destination: 'Travel Destination',
+   destination: 'Travel Destination4',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination5',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination6',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination4',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination7',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination8',
+   date1: "01.01.2020",
+   date2: "02.02.2020",
+   price: 200,
+   duration: 14,
+   descshow: false,
+   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur id eius incidunt, aperiam cum obcaecati! Ex veritatis adipisci, distinctio ipsam quis corporis recusandae incidunt. Ipsum ab magni natus error.'
+ },
+ {
+   destination: 'Travel Destination9',
    date1: "01.01.2020",
    date2: "02.02.2020",
    price: 200,
